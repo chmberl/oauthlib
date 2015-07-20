ee81408d1917b1dc8bfa90ffb17ab00b99e0cd3e
张博良 <chmberl@gmail.com>
2015-07-20 19:05:58 +0800

    update common.py

--- oauthlib/common.py
+++ oauthlib/common.py
@@ -397,6 +397,7 @@ class Request(object):
             "state": None,
             "token": None,
             "user": None,
+            "encoding":None,
         }
         self._params.update(dict(urldecode(self.uri_query)))
         self._params.update(dict(self.decoded_body or []))
